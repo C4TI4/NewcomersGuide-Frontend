b123b2cb12a44b7ad91a9ae91c68e7c9bdb73088
Catia <wolfgangcatia@gmail.com>
2023-11-20 15:32:06 +0100

    footer in progress

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { NextUIProvider } from '@nextui-org/react';
 import App from './App.jsx';
 import './index.css';
+import Footer from './Components/Footer.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NextUIProvider>
       <App />
+      <Footer/>
     </NextUIProvider>
   </React.StrictMode>
 );
