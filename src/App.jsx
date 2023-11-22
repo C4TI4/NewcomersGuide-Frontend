db764ca29c52f2ae402faf576c3a7a1bee8b33d3
KimberlyOrdonez <137154807+KimberlyOrdonez@users.noreply.github.com>
2023-11-22 17:58:36 +0100

    changes before merging main to ki branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
-import { NextUIProvider } from '@nextui-org/react';
-import NavBar from './Components/NavBar';
-import './App.css';
+import React, { useState } from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import NavBar from "./Components/NavBar";
+import Header from "./Components/Header"; // Import Header
+import NewsCarousel from "./Components/NewsFeauture";
+import HomeButtons from "./Components/HomeButtons";
+import CultureFaqs from "./Components/CultureFaqs";
+import "./App.css";
 
 export default function App() {
-  const [count, setCount] = useState(0);
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   const toggleDarkMode = () => {
@@ -13,21 +16,16 @@ export default function App() {
 
   return (
     <NextUIProvider dark={isDarkMode.toString()}>
-      <div>
-        {/* pass isDarkMode and toggleDarkMode as props to NavBar */}
-        <NavBar isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
-      </div>
-      <div className={`app-container ${isDarkMode ? 'dark' : ''}`}>
-        <header>
-          <h1>Header</h1>
-        </header>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            Count is {count}
-          </button>
-          <p>Edit <code>src/App.jsx</code> 3</p>
-        </div>
-        <p className="read-the-docs">Let's get building ladiessss :D</p>
+      <NavBar isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
+      <Header /> {/* Include Header here */}
+      <NewsCarousel />
+      <HomeButtons />
+      <CultureFaqs />
+      {/* NewsCarousel component right below the Header */}
+      {/* Other components or content */}
+      <div className={`app-container ${isDarkMode ? "dark" : ""}`}>
+        {/* Additional parts of your application */}
+        {/* Dark mode toggle */}
         <div className="dark-mode-toggle">
           <label>
             Dark Mode
@@ -42,5 +40,3 @@ export default function App() {
     </NextUIProvider>
   );
 }
-
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { NextUIProvider } from '@nextui-org/react';
-import App from './App.jsx';
-import './index.css';
-import Footer from './Components/Footer.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
+import App from "./App.jsx";
+import "./index.css";
+import Footer from "./Components/Footer.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
-      <Footer/>
+      <App /> {/* App now includes Header and NewsCarousel */}
+      <Footer />
     </NextUIProvider>
   </React.StrictMode>
 );
