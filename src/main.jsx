37c0f2598055df08d6bc85958d031e72389f99de
RockandRolls <livelydev010@gmail.com>
2023-11-22 11:17:39 +0100

    changed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { NextUIProvider } from '@nextui-org/react';
-import App from './App.jsx';
-import './index.css';
-import Footer from './Components/Footer.jsx';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { NextUIProvider } from "@nextui-org/react";
+import App from "./App.jsx";
+import Footer from './Components/Footer.jsx'
+//  import AppContext from "./context/AppContext.jsx";
+import "./index.css";
+import "./App.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <NextUIProvider>
-      <App />
-      <Footer/>
-    </NextUIProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+        <React.StrictMode>
+            <NextUIProvider>
+            {/* <main className="dark text-foreground bg-background"> */}
+               {/*     <AppContext> */}
+                        <App />
+                        <Footer/>
+                   {/* </AppContext> */}
+                {/* </main> */}
+            </NextUIProvider>
+        </React.StrictMode>
+    </BrowserRouter>
+); 
