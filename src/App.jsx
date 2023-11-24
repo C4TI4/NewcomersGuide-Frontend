fa430e24cab9dc6d6c1d98d07032a4df0df96fce
KimberlyOrdonez <137154807+KimberlyOrdonez@users.noreply.github.com>
2023-11-24 14:47:24 +0100

    dont merge this code with main, commented out the feauture CultureFaqs

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
-import Layout from './Pages/Layout';
-import Home from './Pages/Home';
 import { Routes, Route } from "react-router-dom";
+import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
+import ArticleList from "./Components/ArticleList.jsx";
+import SingleArticle from "./Components/SingleArticle.jsx";
 
 export default function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-       
-
+        <Route path="/article" element={<ArticleList />} />
+        <Route path="/article/:id" element={<SingleArticle />} />
       </Route>
     </Routes>
   );
 }
-
-export default App;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,29 @@
+import React, { useState } from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import NavBar from "../Components/NavBar";
+import Header from "../Components/Header";
+import HomeButtons from "../Components/HomeButtons";
+import LookingSomething from "../Components/LookingSomething";
+import Footer from "../Components/Footer";
+// import CultureFaqs from "../Components/CultureFaqs"; // Commented out if not needed
+
 export default function Home() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prev) => !prev);
+  };
+
   return (
-    <>
-      <div className="card">
-        <p>
-          Edit <code>Home.jsx</code>{" "}
-        </p>
+    <NextUIProvider dark={isDarkMode.toString()}>
+      <div className={`app-container ${isDarkMode ? "dark" : ""}`}>
+        <NavBar isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
+        <Header />
+        <HomeButtons />
+        {/* <CultureFaqs /> */} {/* Commented out or removed */}
+        <LookingSomething />
+        <Footer />
       </div>
-      <p className="read-the-docs">Let`s get building ladies :D</p>
-      <div className="dark-mode-toggle"></div>
-    </>
+    </NextUIProvider>
   );
 }
