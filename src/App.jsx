9d9d5742575c801585667ea3b3dea81aa4766b91
Nora Martiny <142009461+NoriesDev@users.noreply.github.com>
2023-11-24 14:00:30 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import  SingleArticle  from './Components/SingleArticle.jsx';
 export default function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout />}>
+      <Route path='/' element={<Layout />} />
 
         <Route index element={<Home />} />
         <Route path="/article" element={<ArticleList />} />
