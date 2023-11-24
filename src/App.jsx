0d76593784396ce57e78f484339521dce115cc26
RockandRolls <livelydev010@gmail.com>
2023-11-24 13:41:45 +0100

    translationPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './Pages/Layout';
 import Home from './Pages/Home';
+import TranslationPage from './Pages/TranslationPage';
 
 export default function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-        <Route index element={<Home />} />
+        <Route index element={<Home />} />       
       </Route>
+      <Route path="/translate" element={<TranslationPage/>} />
     </Routes>
   );
 }
---
+++ src/Components/Translate.jsx
@@ -0,0 +1,11 @@
+const Translate= () => {
+    return (
+        <div className="flex flex-col bg-red-700">
+            <div></div>
+            <div></div>
+
+        </div>
+    )
+}
+
+export default Translate; 
