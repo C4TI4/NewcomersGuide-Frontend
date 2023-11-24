239863232169e87a340f5c9f2affe97b4d907eca
NoriesDev <noriesdevelopment@gmail.com>
2023-11-24 13:51:52 +0100

    commit for update

--- src/Pages/Articles.jsx
+++ src/Pages/Articles.jsx
@@ -1,11 +1,13 @@
 import NavBar from '../Components/NavBar';
 import Footer from '../Components/Footer';
+import ArticleList from '../Components/ArticleList';
+
 
 const Articles = () => {
 return (
     <>
     <NavBar />
-    
+    <ArticleList />
     <Footer/>
     </>
 )
---
+++ src/Pages/SingleArticle.jsx
@@ -0,0 +1,16 @@
+import NavBar from '../Components/NavBar';
+import Footer from '../Components/Footer';
+import SingleArticle from '../Components/SingleArticle';
+
+
+const OneArticle = () => {
+return (
+    <>
+    <NavBar />
+    <SingleArticle />
+    <Footer/>
+    </>
+)
+}
+
+export default OneArticle
