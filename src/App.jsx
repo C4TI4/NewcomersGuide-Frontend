5e8e9f3657962575aaeea4f834ffef23ef365ba5
RockandRolls <livelydev010@gmail.com>
2023-11-30 11:07:22 +0100

    nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ import SingleArticle from "./Pages/SingleArticle";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
-
-      <Route index element={<Home />} />
-      <Route path="/article" element={<Articles />} />
-      <Route path="/article/:id" element={<SingleArticle />} />
-      <Route path="/translate" element={<TranslationPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/article" element={<Articles />} />
+        <Route path="/article/:id" element={<SingleArticle />} />
+        <Route path="/translate" element={<TranslationPage />} />
+      </Route>
     </Routes>
   );
 }
