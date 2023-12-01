cd093d088056a935ef7d216af793653a7eec4e29
KimberlyOrdonez <137154807+KimberlyOrdonez@users.noreply.github.com>
2023-12-01 12:56:20 +0100

    adding new updates from the last merge to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import { ThemeContext } from "./context/ThemeContext"; // Adjust the path as necessary
 import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
 import TranslationPage from "./Pages/TranslationPage";
 import SearchResults from "./Pages/SearchResults";
 import Articles from "./Pages/Articles";
 import SingleArticle from "./Pages/SingleArticle";
+import LoginPage from "./Pages/LoginPage";
+import RegisterPage from "./Pages/RegisterPage";
+
 import InteractiveMap from "./Pages/InteractiveMap";
 import NotFound from "./Pages/NotFound";
 export default function App() {
@@ -17,22 +19,11 @@ export default function App() {
         <Route path="/article" element={<Articles />} />
         <Route path="/article/:id" element={<SingleArticle />} />
         <Route path="/translate" element={<TranslationPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<RegisterPage />} />
         <Route path="/InteractiveMap" element={<InteractiveMap />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
-    <ThemeContext>
-      {" "}
-      {/* Wrap your routes with ThemeContext */}
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/search" element={<SearchResults />} />
-          <Route path="/article" element={<Articles />} />
-          <Route path="/article/:id" element={<SingleArticle />} />
-          <Route path="/translate" element={<TranslationPage />} />
-        </Route>
-      </Routes>
-    </ThemeContext>
   );
 }
