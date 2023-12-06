89a7dad8767d6662ccf8ef2987e98c4c1a6fed6a
KimberlyOrdonez <137154807+KimberlyOrdonez@users.noreply.github.com>
2023-12-06 10:32:28 +0100

    updated feautures with dark mode updated path for the home buttons

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,25 +1,28 @@
 import React from "react";
+import useThemeContext from "../context/ThemeContext";
 
 const ContactForm = () => {
+  const { isDarkMode } = useThemeContext();
+
   return (
     <div
-      className="p-5 shadow-md rounded-lg"
-      style={{
-        backgroundImage:
-          "url('/public/images/claudio-schwarz-pN684G33h_M-unsplash.jpg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
+      className={`p-5 shadow-md rounded-lg ${
+        isDarkMode ? "bg-gray-800" : "bg-white"
+      }`}
     >
-      <div className="bg-white bg-opacity-90 p-5 rounded-lg">
-        <h2 className="text-2xl font-semibold mb-6 text-center text-gray-800">
-          Contact Us
-        </h2>
+      <div
+        className={`p-5 rounded-lg ${
+          isDarkMode ? "text-white" : "text-gray-800"
+        }`}
+      >
+        <h2 className="text-2xl font-semibold mb-6 text-center">Contact Us</h2>
         <form action="#" method="POST">
           <div className="mb-4">
             <label
               htmlFor="name"
-              className="block text-sm font-medium text-gray-700"
+              className={`block text-sm font-medium ${
+                isDarkMode ? "text-gray-300" : "text-gray-700"
+              }`}
             >
               Name
             </label>
@@ -27,14 +30,20 @@ const ContactForm = () => {
               type="text"
               name="name"
               id="name"
-              className="mt-1 p-2 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              className={`mt-1 p-2 block w-full shadow-sm sm:text-sm rounded-md ${
+                isDarkMode
+                  ? "bg-gray-700 border-gray-600 text-white"
+                  : "border-gray-300"
+              }`}
               required
             />
           </div>
           <div className="mb-4">
             <label
               htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
+              className={`block text-sm font-medium ${
+                isDarkMode ? "text-gray-300" : "text-gray-700"
+              }`}
             >
               Email Address
             </label>
@@ -42,14 +51,20 @@ const ContactForm = () => {
               type="email"
               name="email"
               id="email"
-              className="mt-1 p-2 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              className={`mt-1 p-2 block w-full shadow-sm sm:text-sm rounded-md ${
+                isDarkMode
+                  ? "bg-gray-700 border-gray-600 text-white"
+                  : "border-gray-300"
+              }`}
               required
             />
           </div>
           <div className="mb-4">
             <label
               htmlFor="message"
-              className="block text-sm font-medium text-gray-700"
+              className={`block text-sm font-medium ${
+                isDarkMode ? "text-gray-300" : "text-gray-700"
+              }`}
             >
               Message
             </label>
@@ -57,7 +72,11 @@ const ContactForm = () => {
               name="message"
               id="message"
               rows="4"
-              className="mt-1 p-2 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              className={`mt-1 p-2 block w-full shadow-sm sm:text-sm rounded-md ${
+                isDarkMode
+                  ? "bg-gray-700 border-gray-600 text-white"
+                  : "border-gray-300"
+              }`}
               required
             ></textarea>
           </div>
@@ -66,15 +85,26 @@ const ContactForm = () => {
               type="checkbox"
               name="copy"
               id="copy"
-              className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+              className={`h-4 w-4 ${
+                isDarkMode ? "text-indigo-600" : "focus:ring-indigo-500"
+              } border-gray-300 rounded`}
             />
-            <label htmlFor="copy" className="ml-2 block text-sm text-gray-900">
+            <label
+              htmlFor="copy"
+              className={`ml-2 block text-sm ${
+                isDarkMode ? "text-gray-300" : "text-gray-900"
+              }`}
+            >
               Send me a copy of this message
             </label>
           </div>
           <button
             type="submit"
-            className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className={`w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md ${
+              isDarkMode
+                ? "text-white bg-gradient-to-r from-gray-600 to-gray-800 hover:from-gray-700 hover:to-gray-900"
+                : "text-white bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700"
+            } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
           >
             Send
           </button>
